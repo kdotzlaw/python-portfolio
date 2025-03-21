e7ca1009e561c9fc056d4f4b57d0ed8d5490d088
Katrina Dotzlaw <73370331+kdotzlaw@users.noreply.github.com>
2025-03-21 15:02:43 -0500

    num launches & active rocket analysis

--- professional-portfolio/space-race/space-race.py
+++ professional-portfolio/space-race/space-race.py
@@ -13,8 +13,8 @@ pd.options.display.float_format = '{:,.2f}'.format
 
 '''
 PRELIM_EXPLORATION
-Input: original data frame
-Output: prints shape, column names, total NaN per column
+INPUT: original data frame
+OUTPUT: prints shape, column names, total NaN per column
 '''
 def prelim_exploration(df):
     print('Examining data...')
@@ -23,18 +23,51 @@ def prelim_exploration(df):
     print(f'Columns: {df.columns}')
     print('Total NaN per column:')
     print(f'{df.isna().sum()}')
+    print('Duplicates:')
+    print(f'{df.duplicated()}')
       
 '''
 CLEAN
-Input: original dataframe
-Output: returns dataframe with duplicates and NaN values removed
+INPUT: original dataframe
+OUTPUT: returns dataframe with duplicates and NaN values removed
 '''
 def clean(df):
+    print('Cleaning data & purging duplicates...')
     # clean NaN values
     cleaned_df = df.dropna()
-    # clean duplicates
-
+    # clean duplicates (default keeps 1st, does in place)
+    cleaned_df = cleaned_df.drop_duplicates()
     return cleaned_df
+
+'''
+
+'''
+def launches_per_org(cleaned_df):
+    print('Number of launches per organization: \n')
+    data = cleaned_df.Organisation.value_counts()
+    print(data)
+    bar = px.bar(x=data.index, y=data.values)
+    bar.show()
+    return
+
+'''
+'''
+def active_vs_decomissioned(cleaned_df):
+    print('Comparison of active vs decomissioned rockets: \n')
+    # StatusActive and StatusRetired 
+    count = cleaned_df.Rocket_Status.value_counts()
+    print(f'{count}\n')
+    result = count['StatusActive'] - count['StatusRetired']
+    print(f'{result} more Active rockets than Retired rockets')
+    return
+
+'''
+CHOROPLETH
+INPUT
+OUTPUT
+'''
+def choropleth(cleaned_df):
+    return
 
 '''
 MAIN
@@ -48,7 +81,15 @@ def main():
 
     # clean NAN and duplicate data
     cleaned_df = clean(df)
+
+    # descriptive statistics
+    print(f' Descriptive statistics for Space Race data:\n {cleaned_df.describe()}')
+    # Basic Charts & Grouped Charts
+    #launches_per_org(cleaned_df)
+    active_vs_decomissioned(cleaned_df)
    
+  
+
 
     
 
